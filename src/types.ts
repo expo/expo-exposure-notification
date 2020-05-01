cc4b448555afc597c8084f64117b14d36f5076a3
Cedric van Putten <me@bycedric.com>
2020-05-01 20:15:47 +0200

    Update exposure risk level types
    See https://developer.apple.com/documentation/exposurenotification/enrisklevel

--- src/types.ts
+++ src/types.ts
@@ -4,19 +4,17 @@ export type AuthorizationStatus =
   | "restricted"
   | "unknown";
 
-// TODO: The values of these constants are INCORRECT.
-// I could not find the actual values online yet.
 // https://developer.apple.com/documentation/exposurenotification/enrisklevel?language=objc
 export const ExposureRiskLevel = {
-  INVALID: -1,
-  LOWEST: 0,
-  LOW: 1,
-  LOW_MEDIUM: 2,
-  MEDIUM: 3,
-  MEDIUM_HIGH: 4,
-  HIGH: 5,
-  VERY_HIGH: 6,
-  HIGHEST: 7,
+  INVALID: 0,
+  LOWEST: 1,
+  LOW: 10,
+  LOW_MEDIUM: 25,
+  MEDIUM: 50,
+  MEDIUM_HIGH: 65,
+  HIGH: 80,
+  VERY_HIGH: 90,
+  HIGHEST: 100,
 };
 
 export type ExposureKey = {
